08b6c70cbaddaff65c1ca7dd0e2437ddbd7fe618
IndiraKalagara <indirakalagara@IndiraKalagaras-MacBook-Pro.local>
2022-03-31 00:46:20 +0530

    updated region
    Signed-off-by: IndiraKalagara <indirakalagara@IndiraKalagaras-MacBook-Pro.local>

--- scripts/wait-for-cluster-ready.sh
+++ scripts/wait-for-cluster-ready.sh
@@ -4,7 +4,7 @@ SCRIPT_DIR=$(cd $(dirname "$0"); pwd -P)
 
 CLUSTER_NAME="$1"
 REGION="$2"
-
+export AWS_DEFAULT_REGION=${REGION}
 echo "Inputs CLUSTER_NAME  : ${CLUSTER_NAME} , REGION: ${REGION} "
 BIN_DIR=$(cat .bin_dir)
 cat .bin_dir
@@ -13,7 +13,7 @@ count=0
 ${BIN_DIR}/rosa  login 
 ${BIN_DIR}/rosa  init --region=${REGION}
 while true ; do
-  cluster_status=$(${BIN_DIR}/rosa describe cluster --cluster ${CLUSTER_NAME} -o json | ${BIN_DIR}/jq -r ."status.state")
+  cluster_status=$(${BIN_DIR}/rosa describe cluster --cluster ${CLUSTER_NAME} --region=${REGION} -o json | ${BIN_DIR}/jq -r ."status.state")
   
   echo "cluster_status : ${cluster_status}"
   if [[ ${count} -gt 12 ]]; then
