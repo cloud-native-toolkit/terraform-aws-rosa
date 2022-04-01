5c74c6aa77e9b08881b84271613c1146c28ad078
IndiraKalagara <indirakalagara@IndiraKalagaras-MacBook-Pro.local>
2022-04-01 10:05:16 +0530

    updated delete script
    Signed-off-by: IndiraKalagara <indirakalagara@IndiraKalagaras-MacBook-Pro.local>

--- test/stages/stage3-outputs.tf
+++ test/stages/stage3-outputs.tf
@@ -7,14 +7,14 @@ output "ocp_api_server_url" {
 
 }
 
-output "ocp_cluster_admin_user" {
-  value = module.dev_aws_rosa.ocp_cluster_admin_user
+# output "ocp_cluster_admin_user" {
+#   value = module.dev_aws_rosa.ocp_cluster_admin_user
 
-}
-output "ocp_cluster_admin_pwd" {
-  value = module.dev_aws_rosa.ocp_cluster_admin_pwd
+# }
+# output "ocp_cluster_admin_pwd" {
+#   value = module.dev_aws_rosa.ocp_cluster_admin_pwd
 
-}
+# }
 
 output "vpc_id" {
   depends_on = [
